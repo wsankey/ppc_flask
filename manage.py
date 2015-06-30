8c3c641242433c73521d8e21dc022dda0b589e4d
William Sankey <WSankey@gmail.com>
2015-06-29 21:55:08 -0400

    Updated for heroku.

--- manage.py
+++ manage.py
@@ -19,8 +19,8 @@ COV.start()
 from project import app, db
 from project.models import User
 
-app.config.from_object(os.environ['APP_SETTINGS'])
-
+project.app.config.from_object(os.environ['APP_SETTINGS'])
+print os.environ['APP_SETTINGS']
 migrate = Migrate(app, db)
 manager = Manager(app)
 
