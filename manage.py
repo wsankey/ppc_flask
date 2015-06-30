dae527d24085c73a982b6c12b10ea3a32c707c12
William Sankey <WSankey@gmail.com>
2015-06-29 22:08:25 -0400

    Moved secret out of if block.

--- manage.py
+++ manage.py
@@ -80,7 +80,7 @@ def create_admin():
     )
     db.session.commit()
 
-
+app.secret_key = 'super sssssecret'
 if __name__ == '__main__':
     manager.secret_key = 'super sssssecret'
     app.config['SESSION_TYPE'] = 'filesystem'
