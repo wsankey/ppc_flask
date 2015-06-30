b1f5a82817000ae417861509943798f984556409
William Sankey <WSankey@gmail.com>
2015-06-29 21:50:47 -0400

    Testing heroku config.

--- project/__init__.py
+++ project/__init__.py
@@ -22,6 +22,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config.from_object(os.environ['APP_SETTINGS'])
+print os.environ['APP_SETTINGS']
 
 ####################
 #### extensions ####
