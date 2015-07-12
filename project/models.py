1ba73919860d71c36738517a6943499ac0e7f2e4
William Sankey <WSankey@gmail.com>
2015-07-11 20:17:57 -0400

    Creating Products model for Artists.

--- project/models.py
+++ project/models.py
@@ -5,43 +5,6 @@ import datetime
 
 from project import db, bcrypt
 
-
-class User(db.Model):
-
-    __tablename__ = "users"
-
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String, unique=True, nullable=False)
-    password = db.Column(db.String, nullable=False)
-    registered_on = db.Column(db.DateTime, nullable=False)
-    admin = db.Column(db.Boolean, nullable=False, default=False)
-    confirmed = db.Column(db.Boolean, nullable=False, default=False)
-    confirmed_on = db.Column(db.DateTime, nullable=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-
-    def __init__(self, email, password, confirmed,
-                 admin=False, confirmed_on=None):
-        self.email = email
-        self.password = bcrypt.generate_password_hash(password)
-        self.registered_on = datetime.datetime.now()
-        self.admin = admin
-        self.confirmed = confirmed
-        self.confirmed_on = confirmed_on
-
-    def is_authenticated(self):
-        return True
-
-    def is_active(self):
-        return True
-
-    def is_anonymous(self):
-        return False
-
-    def get_id(self):
-        return self.id
-
-    def __repr__(self):
-        return '<email {}'.format(self.email)
 '''
 User Roles:
     Anonymous:  User who is not logged in. Cannot see artists
@@ -55,6 +18,7 @@ class Permission:
     BUY = 0x01
     SELL = 0x02
     ADMINISTER = 0x03
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -80,4 +44,48 @@ class Role(db.Model):
                 role.default = roles[r][1]
                 db.session.add(role)
             db.session.commit()
+
+
+class User(db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    registered_on = db.Column(db.DateTime, nullable=False)
+    admin = db.Column(db.Boolean, nullable=False, default=False)
+    confirmed = db.Column(db.Boolean, nullable=False, default=False)
+    confirmed_on = db.Column(db.DateTime, nullable=True)
+    about_me = db.Column(db.Text())
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    products = db.relationships('Product', backref='artist', lazy=dynamic)
+
+    def __init__(self, email, password, confirmed,
+                 admin=False, confirmed_on=None):
+        self.email = email
+        self.password = bcrypt.generate_password_hash(password)
+        self.registered_on = datetime.datetime.now()
+        self.admin = admin
+        self.confirmed = confirmed
+        self.confirmed_on = confirmed_on
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.id
+
+    def __repr__(self):
+        return '<email {}'.format(self.email)
+
+class Product(db.Model):
+    __tablename__ = "products"
+    id = db.Column(db.Integer, primary_key=True)
 
